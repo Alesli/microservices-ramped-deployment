d4208089f68084d4ce22d96851c50ce6a4c96fb3
Alesia_Skarakhod <Alesia.Skarakhod@wolterskluwer.com>
2023-04-28 17:56:04 +0300

    change README.md

--- post-service/build_docker.sh
+++ post-service/build_docker.sh
@@ -2,11 +2,7 @@
 
 ./mvnw clean package
 docker login --username=epamalesiaskarakhod
-docker build -t postapp-image -f DockerfilePostApp .
-docker tag postapp-image epamalesiaskarakhod/postapp-image:1.0.0
+docker build -t epamalesiaskarakhod/postapp-image:1.0.0 -f DockerfilePostApp .
 docker push epamalesiaskarakhod/postapp-image:1.0.0
 
-docker build -t epamalesiaskarakhod/postrapp-image-1.0.0 ./
-docker login
-docker push epamalesiaskarakhod/postapp-image-1.0.0
-docker run -ti -e DB_URL='jdbc:postgresql://posts-db-container:5433/postsdb' -e DB_USER='root' -e DB_PASSWORD='password' -p 8089:8089 -d epamalesiaskarakhod/userapp-image-1.0.0
+docker run -ti -e DB_URL='jdbc:postgresql://posts-db-container:5432/postsdb' -e DB_USER='root' -e DB_PASSWORD='password' -p 8089:8089 -d epamalesiaskarakhod/postapp-image-1.0.0
